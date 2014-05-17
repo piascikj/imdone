2e0215abd173d684a5d4660c37e2f79291c5738b
Jesse Piascik <jesse@piascik.net>
2014-05-17 16:13:45 -0500

    No need for gzip on socket.io.js

--- server/server.js
+++ server/server.js
@@ -311,7 +311,6 @@
 
     io.enable('browser client minification');  // send minified client
     io.enable('browser client etag');          // apply etag caching logic based on version number
-    io.enable('browser client gzip');          // gzip the file
     io.set('log level', 1);                    // reduce logging
 
     io.sockets.on('connection', function(socket) {
